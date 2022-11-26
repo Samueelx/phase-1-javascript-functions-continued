8c50e7b18fa14ec6081929020ece64feeb0167f6
Samueelx <gsammygitau@gmail.com>
2022-11-26 08:01:47 +0300

    two tests passed.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+console.log(saturdayFun('visit the library'));
+console.log(mondayWork('go to school'));
